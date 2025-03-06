a01f31d9a19761c8fe81f3da799b4189f9d21b8a
Guibert Denis <guibert.denis@yahoo.com>
2025-03-06 19:03:20 +0100

    commit to get missing file from 4-Bootstrap-Allpages

--- src/components/jsx/images.jsx
+++ src/components/jsx/images.jsx
@@ -1,7 +1,28 @@
 import Jhondoe from "../../assets/images/john-doe-about.jpg"
+import Brush from "../../assets/icons-svg/brush.svg"
+import Code from "../../assets/icons-svg/code.svg"
+import Research from "../../assets/icons-svg/research.svg"
 
 export function About(){
     return(
         <img src ={Jhondoe} alt="Jhon Doe working" />
+    )
+}
+
+export function BrushIcon(){
+    return(
+        <img src={Brush} alt="Pinceau" width={24}></img>
+    )
+}
+
+export function CodeIcon(){
+    return(
+        <img src={Code} alt="Code" width={24}></img>
+    )
+} 
+
+export function ResearchIcon(){ 
+    return(
+        <img src={Research} alt="Recherche" width={24}></img>
     )
 }
--- src/components/jsx/text.jsx
+++ src/components/jsx/text.jsx
@@ -1,3 +1,5 @@
+import { BrushIcon, CodeIcon, ResearchIcon } from "./images";
+
 export  function Profil() {
     const message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec feugiat tortor. Proin ac tellus sit amet dolor fringilla elementum.Nullam pharetra imperdiet augue ullamcorper pellentesque. Phasellus convallis, lorem at dictum ultricies, neque tortor posuere mauris, ac dignissim odio risus et nisl. Curabitur molestie tempor tortor, faucibus efficitur massa dictum non. Donec eu tellus in nunc dapibus commodo. Sed ut quam mi. Curabitur suscipit at lorem a dapibus.Nullam sed dapibus justo.Phasellus eu sapien in sapien aliquam interdum vel vel tellus. Vestibulum elementum malesuada dapibus. Aenean ultrices eget risus dictum faucibus. Sed vulputate, justo nec auctor pharetra, massa neque posuere turpis, a posuere lacus libero non mauris. Curabitur ac odio consequat, porta risus non, rutrum odio. Nulla facilisi. Ut tincidunt purus tellus, ac aliquet neque lobortis id. Nullam urna augue, efficitur vel massa in, aliquet sollicitudin augue.Nam libero massa, blandit eget ex ut, pharetra fermentum est. Donec aliquam, tellus non volutpat vehicula, metus libero finibus tortor, at porta ligula felis vitae justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam tincidunt, dolor quis vehicula volutpat, est nisl interdum orci, ac auctor libero tortor sed urna. Donec ut viverra mi, et efficitur nisi. Ut ac velit luctus, iaculis eros volutpat, suscipit quam. Aliquam erat volutpat. Nam in nulla urna. Duis nulla sem, pellentesque vitae diam sit amet, consectetur rhoncus tortor. Donec convallis ipsum sed convallis bibendum. Praesent ut consectetur lorem. Curabitur lobortis bibendum tortor. Etiam id orci ac odio venenatis faucibus. Etiam convallis tincidunt erat et sodales. Vivamus in urna quis lectus varius ornare ac sit amet lacus. Curabitur id dapibus nunc, sagittis tincidunt ipsum. Vestibulum egestas consectetur tempor. Aliquam euismod luctus lectus sit amet vulputate. Etiam feugiat justo vel ipsum fermentum, sagittis auctor ex fermentum.Fusce sed justo in erat pulvinar eleifend vel sed quam.";
     return ( <p>{message}</p> )
@@ -26,3 +28,17 @@ export  function Realisation06() {
     const message ="Maquette d'un site";
     return ( <p>{message}</p> )
 };
+
+export function Design ([title, description]) {
+    return(
+        <div className="">
+            <BrushIcon/>
+            <h2 className="Service-type">UX Design</h2>
+        </div>
+    )
+}
+
+            // <img src={Brush} alt="Pinceau" width={24}></img>
+            // <h2 className="Service-type">UX Design</h2>
+            // <p>L UX Design est une discipline qui consiste à concevoir des produite (site web, applications mobiles, logiciels, objets connectés, etc.) 
+            //     en plaçant l utilisatuer au centre des préoccupations. L objetif est de rendre l expérience utilisateur la plus fluide et la plus agréable possible.</p>
