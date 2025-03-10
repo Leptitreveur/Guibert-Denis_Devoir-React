5395e36adb0b0c78a551a18c1f7b938834ab1258
Guibert Denis <guibert.denis@yahoo.com>
2025-03-10 18:05:46 +0100

    add realisation code with few style

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve : {
+    alias : {
+      '$' : path.resolve(__dirname, './src/components/'),
+      '@' : path.resolve(__dirname, './src/assets/images/portfolio' )
+    }
+  }
 });
