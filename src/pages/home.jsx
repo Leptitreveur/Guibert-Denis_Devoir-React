d8064e63b089820e37e2d2246e43e5fd052080c2
Guibert Denis <guibert.denis@yahoo.com>
2025-06-06 10:08:41 +0200

    Home.jsx name revue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,13 +3,13 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { HeroBg } from "jsx/images"
-import { Profil } from "jsx/text"
-import { SkillsToShow } from 'jsx/skill-section';
+import { BgHero } from "jsx/imageAssets"
+import { ProfilText } from "jsx/textAssets"
+import { ProgressBarList } from 'jsx/progressBar';
 
 // *        MODAL       ===========================================================
 
-export function UserProfile() {
+export function UserProfileModale() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -39,8 +39,8 @@ export function UserProfile() {
     {key :"1",
       iconClass: "bi-person",
       content: (
-        <Link to={user.html_url} target="_blank" rel="noopener noreferrer" className="app_link">
-          <span className="app_link-item">{user.name}</span>
+        <Link to={user.html_url} target="_blank" rel="noopener noreferrer" className="app-link">
+          <span className="app-link__item">{user.name}</span>
         </Link>
       )
     },
@@ -69,32 +69,32 @@ const commonDivClass = "app_modal-contentBox";
 
   return (
     <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div className="modal-dialog modal-dialog-centered modal-xl app_modalDialog">
-        <div className="modal-content app_modal-content">
+      <div className="modal-dialog modal-dialog-centered modal-xl app-modale__dialog">
+        <div className="modal-content app-modal__content">
 
-          <div className="modal-header app_modal-header">
+          <div className="modal-header app-modale__header">
             <h5 className="modal-title" id='staticBackdropLabel'>Mon profil GitHub</h5>
               <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
 
-          <div className="modal-body app_modal-body">
-            <div className="app_imageBox">
-              <img src={user.avatar_url} alt={user.login} className="app_modal-image" />
+          <div className="modal-body app-modale__body">
+            <div className="app-modale__box--image">
+              <img src={user.avatar_url} alt={user.login} className="app-modale__image" />
             </div>
-            <div className="app_infoBox">
+            <div className="app-modale__box--info">
               {paragraphItem.map( (item, index) => (
                 <React.Fragment key={item.key}>
                   <div className= {commonDivClass}>
                     <i className= {`bi ${item.iconClass}`}></i>
                     {item.content}
                   </div>
-                {index !== paragraphItem.length -1 && <hr className="app_hr"/>}
+                {index !== paragraphItem.length -1 && <hr className="app-hr"/>}
                 </React.Fragment>
               ))}
             </div>
           </div>
 
-          <div className='modal-footer app_modal-footer'>
+          <div className='modal-footer app-modale__footer'>
             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close"> Fermer </button>
           </div>
         </div>
@@ -111,23 +111,23 @@ export default function HomePage() {
         <title>Accueil</title>
         <meta name="description" content="Jhon Doe vous propose ses services en qualité de Dév Web Full Stack. Page de présentation."/>
       </Head>
-      <div className="app_introductionContainer">
-        <HeroBg />
-        <div className="app_introductionContainer-box">
-          <h1 className="app_title-1">Bonjour, je suis Jhon Doe</h1>
-          <h2 className="app_title-2">Développeur web full stack</h2>
-          <button className="btn btn-danger customBtn" type="button" data-bs-toggle= "modal" data-bs-target="#staticBackdrop">
+      <div className="app-introduction__container">
+        <BgHero />
+        <div className="app-introduction__box">
+          <h1 className="app-title--1">Bonjour, je suis Jhon Doe</h1>
+          <h2 className="app-title--2">Développeur web full stack</h2>
+          <button className="btn btn-danger app-btn" type="button" data-bs-toggle= "modal" data-bs-target="#staticBackdrop">
             En savoir plus
           </button>
-          <div className="modal-container app_modalContainer">
-            <UserProfile />
+          <div className="modal-container app-modale__container">
+            <UserProfileModale />
           </div>
         </div>
       </div>
 
-      <div className="app_aboutContainer shadow">
-        <Profil />
-        <SkillsToShow />
+      <div className="app-about__container shadow">
+        <ProfilText />
+        <ProgressBarList />
       </div>
     </>
   )
