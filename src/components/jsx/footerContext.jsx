dc0a0e4b2c3add26c57880bb214669a5a85b8d0b
Guibert Denis <guibert.denis@yahoo.com>
2025-06-06 15:02:22 +0200

    footerContext.jsx naming revue

--- src/components/jsx/footerContext.jsx
+++ src/components/jsx/footerContext.jsx
@@ -1,17 +1,17 @@
 import { PropTypes } from "prop-types";
 
-import { FooterStyle } from "jsx/footer-context.jsx";
-import { ContactList } from "jsx/contact-card";
-import { ReaFooterList } from "jsx/realisation-card";
-import { NavbarItems } from "jsx/dynamic-navbar";
+import { FooterStyle } from "jsx/footerContext.jsx";
+import { ContactCardList } from "jsx/contactCard";
+import { PortfolioLinkList } from "jsx/portfolioCard";
+import { NavbarLinkList } from "jsx/navBar";
 
 export function Footer({style=true}) {
     return(
-        <footer className ="footerContainer">
+        <footer className ="app-footer__container">
             <FooterStyle.Provider value={style}>
-                <ContactList selectedIds={["editor"]}/>
-                <NavbarItems />
-                <ReaFooterList />
+                <ContactCardList selectedIds={["editor"]}/>
+                <NavbarLinkList />
+                <PortfolioLinkList />
             </FooterStyle.Provider>
         </footer>
     )
