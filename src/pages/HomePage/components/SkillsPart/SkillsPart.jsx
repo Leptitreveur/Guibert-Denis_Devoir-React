54bdccd16d9396b4e02bc908ad0989ee8076e247
Guibert Denis <guibert.denis@yahoo.com>
2025-08-27 14:53:28 +0200

    update code to stick to rule's project. renaming files and folders SkillSection and ProfilSection to SkillPart and ProfilPart, update comment, add param to PageLayout function.

--- src/pages/HomePage/components/SkillsPart/SkillsPart.jsx
+++ src/pages/HomePage/components/SkillsPart/SkillsPart.jsx
@@ -2,7 +2,7 @@ import { ProgressbarsList } from 'src/components/progressbar/ProgressbarsList/Pr
 import { TitleSection } from 'src/components/common/TitleSection/TitleSection';
 
 /**Titre + liste des barres de progression des compétences.*/
-export const SkillSection = () => {
+export const SkillPart = () => {
   return (
     <div className="col-12 col-md-6">
       <TitleSection title="Mes compétences" />
