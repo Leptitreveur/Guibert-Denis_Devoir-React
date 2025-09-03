25169a0b7fac0d28171961b1d68d642a8645f29a
Guibert Denis <guibert.denis@yahoo.com>
2025-09-03 12:20:44 +0200

    refactorize Navbar.jsx - add a fonction to handle properly active class on current page. test ok

--- src/components/navbar/Navbar/Navbar.jsx
+++ src/components/navbar/Navbar/Navbar.jsx
@@ -5,21 +5,27 @@ import { useContextualStyle } from 'src/hooks/useContextualStyle';
 
 /**
  * Élément de navigation individuel avec lien et styles contextuels
+ * Gère automatiquement l'état actif via React Router et applique les classes CSS appropriées
  *
  * @param {Object} props - Propriétés du composant
  * @param {Object} props.navData - Données de navigation
  * @param {string} props.navData.id - Identifiant unique du lien
  * @param {string} props.navData.path - Chemin de la route
  * @param {string} props.navData.name - Nom affiché du lien
- * @returns {JSX.Element} Élément de navigation avec lien
+ * @returns {JSX.Element} Élément de navigation avec lien avec gestion d'état actif
  */
 export const Navbar = ({ navData }) => {
   const { id, path, name } = navData;
   const { getClassProps } = useContextualStyle();
 
+  const baseLinkClass = { ...getClassProps('link', 'nav') } ?.className || '';
+  const linkClassName = ({isActive}) => {
+    return [baseLinkClass, isActive && 'active'].filter(Boolean).join(' '); 
+  }
+
   return (
     <li data-id={id} {...getClassProps('lign', 'nav')}>
-      <NavLink to={path} end={path === '/'} {...getClassProps('link', 'nav')}>
+      <NavLink to={path} end={path === '/'} className={linkClassName} >
         <strong>{name}</strong>
       </NavLink>
     </li>
